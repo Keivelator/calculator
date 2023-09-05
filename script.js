d8ea33b61babc9635a491e67f5079d498a926f2b
Keivelator <maulidankiki@gmail.com>
2023-09-05 19:36:48 +0700

    added event listeners to number, operator, and equal buttons

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
-let firstNumber = [];
-let secondNumber = [];
-let displayedNumber = [];
+let currentNumber = '';
+let operator;
+let firstNumber;
+let secondNumber;
+let result;
+
+//Target UI Elements
+const displayTop = document.querySelector('.display-top');
 
 const numberButtons = document.querySelectorAll('.number');
 numberButtons.forEach(numberButton => {
     numberButton.addEventListener('click', () => {
-        firstNumber.push(numberButton.id);
-        
+        displayTop.textContent += numberButton.id;
+        currentNumber += numberButton.id;
+        console.log(currentNumber);
     })
 });
 
+const operatorButtons = document.querySelectorAll('.operator');
+operatorButtons.forEach(operatorButton => {
+    operatorButton.addEventListener('click', () => {
+        operator = operatorButton.textContent;
+        console.log(operator);        
+    })
+})
+
+const equalButton = document.querySelector('.equal');
+equalButton.addEventListener('click', () => {
+    result = operate(firstNumber, secondNumber, operator);
+})
 
 
 
@@ -19,8 +37,9 @@ numberButtons.forEach(numberButton => {
 
 
 
-function operate (num1, num2, operator) {
-    switch (operator) {
+
+function operate (num1, num2, opt) {
+    switch (opt) {
         case '+':
             return num1 + num2;
         case '-':
